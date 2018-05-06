4db0822c4fa03d11866ad82f3b9a0fcad49ddd8e
Jimi Pajala <jimipajala@hotmail.com>
2018-05-07 00:15:55 +0300

    Add default imports back because clock is 00:15

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
 export * from './Compile'
 export * from './Consumer'
 export * from './Provider'
+
+import makeDictionary from './Compile'
+import ConsumeLanguage from './Consumer'
+import withLanguageContext from './Provider'
+
+export {
+  ConsumeLanguage,
+  withLanguageContext,
+  makeDictionary,
+}
