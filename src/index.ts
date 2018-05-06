76224e4178ecc2e621474143cfe38704c4e198fd
Jimi Pajala <jimipajala@hotmail.com>
2018-05-07 00:33:00 +0300

    Push working to latest

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
-export * from './Compile'
-export * from './Consumer'
-export * from './Provider'
-
 import makeDictionary from './Compile'
 import ConsumeLanguage from './Consumer'
 import withLanguageContext from './Provider'
