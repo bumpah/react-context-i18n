02d7e0a00de270e394794a55db4c7aa8c6b7404a
Bumpah <jimipajala@hotmail.com>
2018-10-29 00:26:23 +0200

    clean up Compile.tsx file

--- src/Compile.tsx
+++ src/Compile.tsx
@@ -6,27 +6,18 @@ export interface DictionaryProps {
 
 // const compileJson = (array: Props): object => {
 export function makeDictionary(array: DictionaryProps[]) {
-  let defaulted: boolean = false
-
+  let defaulted = false
   const json: {} = array.reduce((acc: {}, cur: DictionaryProps): {} => {
-
-    if (!cur.context && defaulted) {
-      return acc
-    }
-
-    if (!cur.context) {
+    if (!cur.context && !defaulted) {
       defaulted = true
       const def: {} = cur.json
       return { ...acc, default: def }
     }
 
-    if (cur.file) {
-      return { ...acc, [cur.context]: (cur.file) }
+    if (cur.context && (cur.file || cur.json)) {
+      return { ...acc, [cur.context]: (cur.file || cur.json) }
     }
 
-    if (cur.json) {
-      return { ...acc, [cur.context]: (cur.json) }
-    }
     return { ...acc }
   }, {})
 
