70119d311ff5bcab718e8a6d71301a7e0907e9a2
Bumpah <jimipajala@hotmail.com>
2018-10-29 00:27:59 +0200

    clean up Compile.tsx

--- src/Compile.tsx
+++ src/Compile.tsx
@@ -7,15 +7,16 @@ export interface DictionaryProps {
 // const compileJson = (array: Props): object => {
 export function makeDictionary(array: DictionaryProps[]) {
   let defaulted = false
+  // Create translations JSON-file
   const json: {} = array.reduce((acc: {}, cur: DictionaryProps): {} => {
     if (!cur.context && !defaulted) {
       defaulted = true
-      const def: {} = cur.json
+      const def: {} = cur.json || cur.file
       return { ...acc, default: def }
     }
 
     if (cur.context && (cur.file || cur.json)) {
-      return { ...acc, [cur.context]: (cur.file || cur.json) }
+      return { ...acc, [cur.context]: (cur.json || cur.file) }
     }
 
     return { ...acc }
