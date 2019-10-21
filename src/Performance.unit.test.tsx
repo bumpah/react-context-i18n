03287bf2c7791113a851138d2d321180054f0bd4
Jimi Pajala <jimipajala@hotmail.com>
2019-10-21 11:44:55 +0300

    fix build build type errors and update dev packages

--- src/Performance.unit.test.tsx
+++ src/Performance.unit.test.tsx
@@ -6,14 +6,14 @@ import withProvide from '../src/Provider'
 
 import getTrans from '../translations'
 
-test('calculate performance', async () => {
+test('calculate performance', () => {
   const Wrapper = () => (<Trans text="name" />)
   const Component = withProvide(getTrans('fi'))(Wrapper)
   const tree = renderer.create(<Component />)
   // const root = tree.root
   const now = new Date().getTime()
   for (let i = 0; i < 10000; i++) {
-    await tree.update(<Component translations={getTrans('fi')} />)
+    tree.update(<Component translations={getTrans('fi')} />)
   }
 
   console.log(new Date().getTime() - now, 'ms')
